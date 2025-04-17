d5561a845dc18fb36c72a0ba9e1a1472a196ae2a
Erik Saule <esaule@uncc.edu>
2025-04-17 15:18:30 -0400

    missing java file

---
+++ mockup/jarfileWithJNI/somethingelse/SomethingJarWithJNI.java
@@ -0,0 +1,12 @@
+package somethingelse;
+
+public class SomethingJarWithJNI {
+    static {
+	System.out.println("idonsaiod");
+	//System.load("/home/erik/work/gephi-plugins/mockup/jarfileWithJNI/libnative.so");
+	System.loadLibrary("native");
+	System.out.println("idonsaiod");
+    }
+    
+    public static native void test ();
+}
